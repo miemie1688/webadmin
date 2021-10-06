6b85887cd4d23a4a5f05bf7c6b535f6cc4cff854
miemie1688 <42808093+miemie1688@users.noreply.github.com>
2021-10-07 00:57:37 +0800

    Initial commit

---
+++ src/main/java/com/spring/boot/webadmin/WebadminApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.webadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebadminApplication.class, args);
+    }
+
+}
